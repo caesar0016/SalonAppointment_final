6e9e4da518c47ce20d31edd4454adc1cb284946e
caesar0016 <dapitoncaesar26@gmail.com>
2024-03-28 16:45:51 +0800

    Pang abang for category registraion with img. Felt sleepy and muddle headed so I need to have a power nap lol

--- app/src/main/java/com/example/salonappointment/registration/categories_registration_frm.java
+++ app/src/main/java/com/example/salonappointment/registration/categories_registration_frm.java
@@ -1,6 +1,10 @@
 package com.example.salonappointment.registration;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +13,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.salonappointment.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class categories_registration_frm extends AppCompatActivity {
+
+    private ImageView imgCategory;
+    private EditText edCategoryName, edCategoryDesc;
+    private Button btnRegisterCategory;
+    private DatabaseReference dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +33,21 @@ public class categories_registration_frm extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Initialization of the components
+        imgCategory = findViewById(R.id.add_img_category);
+        edCategoryName = findViewById(R.id.etv_signUpFrm_name);
+        edCategoryDesc = findViewById(R.id.etvDesc_categoriesRegistration);
+        btnRegisterCategory = findViewById(R.id.btnRegisterCategory);
+
+        dbRef = FirebaseDatabase.getInstance().getReference().child("Service Category");
+
+        btnRegisterCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
