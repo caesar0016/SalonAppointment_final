f5251bffed6389788218fa9179b4b9d367ed8eea
caesar0016 <dapitoncaesar26@gmail.com>
2024-03-28 21:59:02 +0800

    Fully Working categories_registration_frm with picture uploading. Need to add progress bar and more polishing and optimizing

--- app/src/main/java/com/example/salonappointment/loginFrm.java
+++ app/src/main/java/com/example/salonappointment/loginFrm.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,9 +13,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.salonappointment.registration.categories_registration_frm;
+
 public class loginFrm extends AppCompatActivity {
     private Button btnLogin_loginFrm;
     private TextView signUp;
+    private ImageView img_Google, img_ig, img_fb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +33,18 @@ public class loginFrm extends AppCompatActivity {
         //initialization find by View ID
         btnLogin_loginFrm = findViewById(R.id.btn_loginFrm_Login);
         signUp = findViewById(R.id.signupFrm_loginDirect);
+        img_Google = findViewById(R.id.img_loginFrm_icGoogle);
+        img_ig = findViewById(R.id.img_LoginFrm_icInstagram);
+        img_fb = findViewById(R.id.img_LoginFrm_icFacebook);
+
+
+        img_Google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(loginFrm.this, categories_registration_frm.class);
+                startActivity(intent);
+            }
+        });
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/salonappointment/registration/categories_registration_frm.java
+++ app/src/main/java/com/example/salonappointment/registration/categories_registration_frm.java
@@ -1,20 +1,33 @@
 package com.example.salonappointment.registration;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.salonappointment.Model.category_registration_model;
 import com.example.salonappointment.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 public class categories_registration_frm extends AppCompatActivity {
 
@@ -22,6 +35,10 @@ public class categories_registration_frm extends AppCompatActivity {
     private EditText edCategoryName, edCategoryDesc;
     private Button btnRegisterCategory;
     private DatabaseReference dbRef;
+    private StorageReference storageRef;
+    private static String url;
+
+    private Uri selectedImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +56,62 @@ public class categories_registration_frm extends AppCompatActivity {
         edCategoryName = findViewById(R.id.etv_signUpFrm_name);
         edCategoryDesc = findViewById(R.id.etvDesc_categoriesRegistration);
         btnRegisterCategory = findViewById(R.id.btnRegisterCategory);
-
+        storageRef = FirebaseStorage.getInstance().getReference();
         dbRef = FirebaseDatabase.getInstance().getReference().child("Service Category");
 
+
+        //this goes the events
+        imgCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+        //btnRegisterCategory set on click event
         btnRegisterCategory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                uploadPicture();
+                String category = edCategoryName.getText().toString().trim();
+                String description = edCategoryDesc.getText().toString().trim();
+                category_registration_model r1 = new category_registration_model(category, description, url);
+                dbRef.push().setValue(r1);
             }
         });
+    }
+    private void chooseImage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null){
+            selectedImageUri = data.getData();
+            imgCategory.setImageURI(selectedImageUri);
+        }
+    }
 
+    private void uploadPicture() {
+        if(selectedImageUri != null){
+            StorageReference imageRef = storageRef.child("images/" + UUID.randomUUID().toString());
+            url = imageRef.toString();
+            imageRef.putFile(selectedImageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(categories_registration_frm.this, "Upload Successful", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(categories_registration_frm.this, "Upload Failed" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }else{
+            Toast.makeText(this, "No Image Selected", Toast.LENGTH_SHORT).show();
+        }
     }
 }
