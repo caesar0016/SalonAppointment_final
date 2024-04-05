bae49938a8d5c54ffb0be5065f0a5cf47168f0a8
caesar0016 <dapitoncaesar26@gmail.com>
2024-04-05 16:31:04 +0800

    service registration optimize design and latest api for choosing the pictures.

--- app/src/main/java/com/example/salonappointment/Model/register_service_model.java
+++ app/src/main/java/com/example/salonappointment/Model/register_service_model.java
@@ -2,9 +2,55 @@ package com.example.salonappointment.Model;
 
 public class register_service_model {
 
-    private String serviceName, photos;
+    private String photos, serviceName, description;
     private int duration;
     private double price;
 
+    public register_service_model(String photos, String serviceName, String description, int duration, double price) {
+        this.photos = photos;
+        this.serviceName = serviceName;
+        this.description = description;
+        this.duration = duration;
+        this.price = price;
+    }
 
+    public String getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(String photos) {
+        this.photos = photos;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- app/src/main/java/com/example/salonappointment/registration/service_registration_frm.java
+++ app/src/main/java/com/example/salonappointment/registration/service_registration_frm.java
@@ -1,15 +1,24 @@
 package com.example.salonappointment.registration;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.NumberPicker;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -32,8 +41,10 @@ public class service_registration_frm extends AppCompatActivity {
     private DatabaseReference dbRef;
     private ArrayList<String> spinnerList;
     private ArrayAdapter<String> adapter;
-    private EditText ed01_category;
     private Button btnRegister;
+    private ImageView imgService;
+    private Uri uri;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,44 +59,75 @@ public class service_registration_frm extends AppCompatActivity {
 
         //initialization
         spinnerCategory = findViewById(R.id.serviceReg_spinner);
-        dbRef = FirebaseDatabase.getInstance().getReference("Service Category");
         spinnerList = new ArrayList<>();
-        ed01_category = findViewById(R.id.ed_Category01);
         btnRegister = findViewById(R.id.serviceReg_btnRegister);
+        NumberPicker hrPicker = findViewById(R.id.hoursPicker);
+        NumberPicker mnPicker = findViewById(R.id.minutesPicker);
+        progressBar = findViewById(R.id.pbProgress);
+        imgService = findViewById(R.id.add_img_service);
+
+        //picker Initialization
+        hrPicker.setMinValue(1);
+        hrPicker.setMaxValue(10);
+
+        //minute Picker Initialization
+        mnPicker.setMinValue(1);
+        mnPicker.setMaxValue(60);
 
         adapter = new ArrayAdapter<>(service_registration_frm.this, android.R.layout.simple_spinner_dropdown_item, spinnerList);
-        btnRegister.setOnClickListener(new View.OnClickListener() {
+        imgService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String value = ed01_category.getText().toString();
-                String key = dbRef.push().getKey();
-                dbRef.child(key).setValue(value);
-                ed01_category.setText(" ");
-                spinnerList.clear();
-                adapter.notifyDataSetChanged();
-
-                Toast.makeText(service_registration_frm.this, "Saved", Toast.LENGTH_SHORT).show();
+                chooseImage();
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() { //btnRegister Click Event
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                Toast.makeText(service_registration_frm.this, "Saved Successfully", Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.INVISIBLE);
             }
         });
         spinnerCategory.setAdapter(adapter);
         showData();
     }
+    ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
+            registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+                // Callback is invoked after the user selects a media item or closes the
+                // photo picker.
+                if (uri != null) {
+                    Log.d("PhotoPicker", "Selected URI: " + uri);
+                } else {
+                    Log.d("PhotoPicker", "No media selected");
+                }
+            });
+    private void chooseImage(){
+        pickMedia.launch(new PickVisualMediaRequest());
+    }
 
-    private void showData() {
+
+    private void InsertService(String photos, String name, String Description, int duration, double price){
+        dbRef = FirebaseDatabase.getInstance().getReference("Service");
+
+    }
+
+    private void showData() {//gets the category name from the firebase
+        dbRef = FirebaseDatabase.getInstance().getReference("Service Category");
         dbRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                progressBar.setVisibility(View.VISIBLE);
                 for(DataSnapshot item : snapshot.getChildren()){
 
                     spinnerList.add(item.getValue().toString());
-
                 }
                 adapter.notifyDataSetChanged();
+                progressBar.setVisibility(View.INVISIBLE);
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                //do Nothing
             }
         });
     }
