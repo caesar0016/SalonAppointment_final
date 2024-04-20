9ea840bf81fa309e3b0437f6d6270d4bc6ec718d
caesar0016 <dapitoncaesar26@gmail.com>
2024-04-20 09:24:05 +0800

    Re-activated login method

--- app/src/main/java/com/example/salonappointment/loginFrm.java
+++ app/src/main/java/com/example/salonappointment/loginFrm.java
@@ -77,21 +77,21 @@ public class loginFrm extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(loginFrm.this, main_page_frm.class);
-                startActivity(intent);
+//                Intent intent = new Intent(loginFrm.this, main_page_frm.class);
+//                startActivity(intent);
 
-//                String email = login_email.getText().toString().trim();
-//                String password = login_password.getText().toString().trim();
-//
-//                if(TextUtils.isEmpty(email)){
-//                    login_email.setError("Email is Required");
-//                    return;
-//                }
-//                if(TextUtils.isEmpty(password)){
-//                    login_password.setError("Password is Required");
-//                    return;
-//                }
-//                accountLogin(email, password);
+                String email = login_email.getText().toString().trim();
+                String password = login_password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    login_email.setError("Email is Required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    login_password.setError("Password is Required");
+                    return;
+                }
+                accountLogin(email, password);
             }
         });
     }
