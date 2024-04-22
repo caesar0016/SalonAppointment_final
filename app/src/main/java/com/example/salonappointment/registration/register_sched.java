dcecfc2d259346e88cce86555cb48ba28c9b7cd2
caesar0016 <dapitoncaesar26@gmail.com>
2024-04-22 17:39:11 +0800

    Spinner for am and pm is working.

--- app/src/main/java/com/example/salonappointment/registration/register_sched.java
+++ app/src/main/java/com/example/salonappointment/registration/register_sched.java
@@ -1,6 +1,11 @@
 package com.example.salonappointment.registration;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +15,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.salonappointment.R;
 
+import java.util.ArrayList;
+
 public class register_sched extends AppCompatActivity {
+
+    private Spinner spin_ampm01, spin_amp02;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,57 @@ public class register_sched extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        //-------------- First Spinner Initialization --------------
+        spin_ampm01 = findViewById(R.id.ampmSpinner01);
+        firstSpin();
+
+        ArrayList<String> array_ampm = new ArrayList<>();
+        array_ampm.add("am");
+        array_ampm.add("pm");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item, array_ampm
+        );
+        adapter.setDropDownViewResource(
+                android.R.layout.select_dialog_singlechoice
+        );
+        spin_ampm01.setAdapter(adapter);
+
+        //--------------Second Spinner Initialization--------------
+        spin_amp02 = findViewById(R.id.ampmSpinner02);
+        secondSpin();
+        spin_amp02.setAdapter(adapter);
+
+    }
+
+    private void firstSpin() {
+        spin_ampm01.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                String item = adapterView.getItemAtPosition(position).toString();
+                Toast.makeText(register_sched.this, "Selected Item: " + item, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void secondSpin() {
+        spin_amp02.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(register_sched.this, "Selected Item: " + item, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
         });
     }
 }
