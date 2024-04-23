4e607f44be7f51d360fd9cba32e04243493ca138
caesar0016 <dapitoncaesar26@gmail.com>
2024-04-23 18:30:19 +0800

    Working database for sched. Still need to fix it where it only push under one uid.

---
+++ app/src/main/java/com/example/salonappointment/Model/staffSched_model.java
@@ -0,0 +1,57 @@
+package com.example.salonappointment.Model;
+
+public class staffSched_model {
+    private String uid;
+    private String startTime;
+    private String startAmOrPm;
+    private String endTime;
+    private String endAmOrPm;
+
+    public staffSched_model(String uid, String startTime, String startAmOrPm, String endTime, String endAmOrPm) {
+        this.uid = uid;
+        this.startTime = startTime;
+        this.startAmOrPm = startAmOrPm;
+        this.endTime = endTime;
+        this.endAmOrPm = endAmOrPm;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getStartAmOrPm() {
+        return startAmOrPm;
+    }
+
+    public void setStartAmOrPm(String startAmOrPm) {
+        this.startAmOrPm = startAmOrPm;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getEndAmOrPm() {
+        return endAmOrPm;
+    }
+
+    public void setEndAmOrPm(String endAmOrPm) {
+        this.endAmOrPm = endAmOrPm;
+    }
+}
--- app/src/main/java/com/example/salonappointment/registration/register_sched.java
+++ app/src/main/java/com/example/salonappointment/registration/register_sched.java
@@ -1,9 +1,12 @@
 package com.example.salonappointment.registration;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -13,13 +16,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.salonappointment.Model.staffSched_model;
 import com.example.salonappointment.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class register_sched extends AppCompatActivity {
 
     private Spinner spin_ampm01, spin_amp02;
+    private EditText edStartTime, edEndTime;
+    private Button btnAdd;
+    String spinAmOrPm = "";
+    String spinAmOrPm2 = "";
+    String uid = "1";
+    private DatabaseReference dbRefSched;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +43,12 @@ public class register_sched extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //Initialization
+        btnAdd = findViewById(R.id.regSched_btnAdd);
+        edStartTime = findViewById(R.id.regSchedstartTime);
+        edEndTime = findViewById(R.id.regSched_endTime);
+        dbRefSched = FirebaseDatabase.getInstance().getReference().child("Staff_Schedule");
+
         //-------------- First Spinner Initialization --------------
         spin_ampm01 = findViewById(R.id.ampmSpinner01);
         firstSpin();
@@ -53,14 +71,37 @@ public class register_sched extends AppCompatActivity {
         secondSpin();
         spin_amp02.setAdapter(adapter);
 
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String startTime = edStartTime.getText().toString().trim();
+                String endTime = edEndTime.getText().toString().trim();
+
+                if (TextUtils.isEmpty(startTime)) {
+                    edStartTime.setError("Please input starting time");
+                    edStartTime.requestFocus();
+                    return;
+                }
+                if (TextUtils.isEmpty(endTime)) {
+                    edEndTime.setError("Please input end time");
+                    edEndTime.requestFocus();
+                    return;
+                }
+                staffSched_model staffModel = new staffSched_model(uid, startTime, spinAmOrPm, endTime, spinAmOrPm2);
+                dbRefSched.push().setValue(staffModel);
+                clearFields();
+                Toast.makeText(register_sched.this, "Success Transaction", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     private void firstSpin() {
         spin_ampm01.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-                String item = adapterView.getItemAtPosition(position).toString();
-                Toast.makeText(register_sched.this, "Selected Item: " + item, Toast.LENGTH_SHORT).show();
+                spinAmOrPm = adapterView.getItemAtPosition(position).toString();
+                Toast.makeText(register_sched.this, "Selected Item: " + spinAmOrPm, Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -74,13 +115,19 @@ public class register_sched extends AppCompatActivity {
         spin_amp02.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String item = parent.getItemAtPosition(position).toString();
-                Toast.makeText(register_sched.this, "Selected Item: " + item, Toast.LENGTH_SHORT).show();
+                spinAmOrPm2 = parent.getItemAtPosition(position).toString();
+                Toast.makeText(register_sched.this, "Selected Item: " + spinAmOrPm2, Toast.LENGTH_SHORT).show();
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
+    }
+
+    private void clearFields() {
+        edStartTime.setText("");
+        edEndTime.setText("");
     }
 }
