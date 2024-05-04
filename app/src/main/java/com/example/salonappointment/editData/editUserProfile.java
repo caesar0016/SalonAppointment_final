11680ea47ab6a82a19d8259cdaf1c1b50c64e030
caesar0016 <dapitoncaesar26@gmail.com>
2024-05-04 21:32:16 +0800

    Editing usertype is working.

--- app/src/main/java/com/example/salonappointment/editData/editUserProfile.java
+++ app/src/main/java/com/example/salonappointment/editData/editUserProfile.java
@@ -1,13 +1,15 @@
 package com.example.salonappointment.editData;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,8 +17,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.bumptech.glide.Glide;
 import com.example.salonappointment.R;
-import com.example.salonappointment.registration.stylist_registration_frm;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -26,6 +33,8 @@ public class editUserProfile extends AppCompatActivity {
     private Spinner spinnerUserTypes;
     private TextInputEditText inputName, inputEmail, inputDesc;
     private CircleImageView profileUrl;
+    private Button btnSave;
+    private static String selectedUserType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +50,9 @@ public class editUserProfile extends AppCompatActivity {
         inputName = (TextInputEditText) findViewById(R.id.etv_loginFrm_name);
         inputEmail = (TextInputEditText) findViewById(R.id.edEmail);
         profileUrl = (CircleImageView) findViewById(R.id.editUserProfile_img);
+        btnSave = (Button) findViewById(R.id.editUser_btnSave);
 
-        inputName.setText(getIntent().getStringExtra("name"));
+        String email = getIntent().getStringExtra("email");
         inputEmail.setText(getIntent().getStringExtra("email"));
 
         Glide.with(this).load(getIntent().getStringExtra("imgUrl")).into(profileUrl);
@@ -55,7 +65,7 @@ public class editUserProfile extends AppCompatActivity {
         array_UserType.add("Hair Stylist");
         array_UserType.add("Nail Technicians");
         array_UserType.add("Makeup Artist");
-        array_UserType.add("Message Therapist");
+        array_UserType.add("Massage Therapist");
         array_UserType.add("Cashier");
         array_UserType.add("Manager");
         array_UserType.add("Admin");
@@ -69,24 +79,44 @@ public class editUserProfile extends AppCompatActivity {
                 android.R.layout.select_dialog_singlechoice
         );
         spinnerUserTypes.setAdapter(adapterSpin);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUser(email, selectedUserType);
+                Toast.makeText(editUserProfile.this, "Finish", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        Intent intent = new Intent(this, stylist_registration_frm.class);
-        startActivity(intent);
+    private void updateUser(String email, String userType) {
+        DatabaseReference accountRef = FirebaseDatabase.getInstance().getReference("User Accounts");
 
-        // Finish the current activity if necessary
-        finish();
+        Query query = accountRef.orderByChild("email").equalTo(email);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot item : snapshot.getChildren()) {
+                    // Update the userType field for the specific user found in the query
+                    String userID = item.getKey();
+                    accountRef.child(userID).child("userType").setValue(userType);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handles Error
+            }
+        });
     }
+
 
     private void userType_spin() {
         spinnerUserTypes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String item = parent.getItemAtPosition(position).toString();
-                // Toast.makeText(editUserProfile.this, "Selected: " + item, Toast.LENGTH_SHORT).show();
+                selectedUserType = parent.getItemAtPosition(position).toString();
+                Toast.makeText(editUserProfile.this, "Selected: " + selectedUserType, Toast.LENGTH_SHORT).show();
             }
 
             @Override
