abe97d33c4d157d4e52cc4e9b34046e092ab314e
caesar0016 <94184309+caesar0016@users.noreply.github.com>
2024-06-07 18:59:19 +0800

    writeReviews stars update

--- app/src/main/java/com/example/salonappointment/registration/write_reviews.java
+++ app/src/main/java/com/example/salonappointment/registration/write_reviews.java
@@ -13,7 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.salonappointment.R;
 
 public class write_reviews extends AppCompatActivity {
-    private ImageView btnBack;
+    private ImageView btnBack, starOne, starTwo, starThree, starFour, starFive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +25,80 @@ public class write_reviews extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //------- This is the initialization ---------------
         btnBack = (ImageView) findViewById(R.id.btnBack);
+
+        //----- Initialization of Stars
+        starOne = (ImageView) findViewById(R.id.wr_starOne);
+        starTwo = (ImageView) findViewById(R.id.wr_starTwo);
+        starThree = (ImageView) findViewById(R.id.wr_starThree);
+        starFour = (ImageView) findViewById(R.id.wr_starFour);
+        starFive = (ImageView) findViewById(R.id.wr_starFive);
+
+        starOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                starOne.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_stars_yellow));
+                starTwo.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_rate_star));
+                starThree.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_rate_star));
+                starFour.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_rate_star));
+                starFive.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_rate_star));
+            }
+        });
+        starTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                starOne.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_stars_yellow));
+                starTwo.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_stars_yellow));
+                starThree.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_rate_star));
+                starFour.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_rate_star));
+                starFive.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_rate_star));
+            }
+        });
+        starThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                starOne.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_stars_yellow));
+                starTwo.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_stars_yellow));
+                starThree.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_stars_yellow));
+                starFour.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_rate_star));
+                starFive.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_rate_star));
+            }
+        });
+        starFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                starOne.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_stars_yellow));
+                starTwo.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_stars_yellow));
+                starThree.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_stars_yellow));
+                starFour.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_stars_yellow));
+            }
+        });
+        starFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                starOne.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_stars_yellow));
+                starTwo.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_stars_yellow));
+                starThree.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_stars_yellow));
+                starFour.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_stars_yellow));
+                starFive.setImageDrawable(v.getContext().getDrawable(R.drawable.ic_stars_yellow));
+            }
+        });
+
+
+
+
+
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+    }
+    private void starToYellow(){
+
+
     }
 }
