b96db307511d54170c59a70bbee085dca0d5821d
caesarDapiton <dapitoncaesar26@gmail.com>
2024-06-10 11:21:53 +0800

    Able to fix the structure of the writeReviewsModel.java need more polishing and add recyclerview for writeReviews

--- app/src/main/java/com/example/salonappointment/Model/writeReviewsModel.java
+++ app/src/main/java/com/example/salonappointment/Model/writeReviewsModel.java
@@ -1,4 +1,57 @@
 package com.example.salonappointment.Model;
 
 public class writeReviewsModel {
+    private String staffUID;
+    private String customerID;
+    private String title;
+    private String description;
+    private int scoreRating;
+
+    public writeReviewsModel(String staffUID, String customerID, String title, String description, int scoreRating) {
+        this.staffUID = staffUID;
+        this.customerID = customerID;
+        this.title = title;
+        this.description = description;
+        this.scoreRating = scoreRating;
+    }
+
+    public String getStaffUID() {
+        return staffUID;
+    }
+
+    public void setStaffUID(String staffUID) {
+        this.staffUID = staffUID;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getScoreRating() {
+        return scoreRating;
+    }
+
+    public void setScoreRating(int scoreRating) {
+        this.scoreRating = scoreRating;
+    }
 }
