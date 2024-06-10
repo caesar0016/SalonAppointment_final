ee7b751906242b9e669de6426fdd9753a58d502d
caesarDapiton <dapitoncaesar26@gmail.com>
2024-06-10 14:23:37 +0800

    Able to display the reviews. Need to connect it with the firebase.

--- app/src/main/java/com/example/salonappointment/adapter/reviews_adapter.java
+++ app/src/main/java/com/example/salonappointment/adapter/reviews_adapter.java
@@ -1,31 +1,66 @@
 package com.example.salonappointment.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class reviews_adapter extends RecyclerView.Adapter<reviews_adapter.ViewHolder>{
+import com.example.salonappointment.Model.writeReviewsModel;
+import com.example.salonappointment.R;
+
+import java.util.ArrayList;
+
+public class reviews_adapter extends RecyclerView.Adapter<reviews_adapter.ViewHolder> {
+    ArrayList<writeReviewsModel> listReviews = new ArrayList<>();
+    private Context context;
+
+    public reviews_adapter(ArrayList<writeReviewsModel> listReviews, Context context) {
+        this.listReviews = listReviews;
+        this.context = context;
+    }
+
+    public reviews_adapter(ArrayList<writeReviewsModel> listReviews) {
+        this.listReviews = listReviews;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.reviews_view_rows, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.tvName.setText(listReviews.get(position).getCustomerID());
+        holder.tvDesc.setText(listReviews.get(position).getDescription());
+        holder.tvDate.setText(listReviews.get(position).getDate());
+        holder.tvRate.setText(String.valueOf(listReviews.get(position).getScoreRating()));
+        holder.tvTitle.setText(listReviews.get(position).getTitle());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listReviews.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvName, tvDesc, tvDate, tvRate, tvTitle;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            tvName = itemView.findViewById(R.id.customerName);
+            tvDesc = itemView.findViewById(R.id.reviewDesc);
+            tvDate = itemView.findViewById(R.id.datePosted);
+            tvRate = itemView.findViewById(R.id.scoreRate);
+            tvTitle = itemView.findViewById(R.id.reviewTitle);
+
         }
     }
 }
--- app/src/main/java/com/example/salonappointment/displayDetail/displayReviews.java
+++ app/src/main/java/com/example/salonappointment/displayDetail/displayReviews.java
@@ -11,15 +11,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.salonappointment.Model.writeReviewsModel;
 import com.example.salonappointment.R;
+import com.example.salonappointment.adapter.reviews_adapter;
 import com.example.salonappointment.registration.write_reviews;
+
+import java.util.ArrayList;
 
 public class displayReviews extends AppCompatActivity {
     private Button btnWriteReviews;
     private ImageView btnBack;
     private RecyclerView rvReviews;
+    private reviews_adapter adapterReviews;
+    private ArrayList<writeReviewsModel> listReviews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +38,20 @@ public class displayReviews extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //------- Initializations
         btnWriteReviews = (Button) findViewById(R.id.dsReview_btnWrite);
         btnBack = (ImageView) findViewById(R.id.btnBack);
+
+        //   ------ Initializations for RecyclerView ---------
+        rvReviews = (RecyclerView) findViewById(R.id.dsReview_rv);
+        LinearLayoutManager layoutReviews = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        rvReviews.setLayoutManager(layoutReviews);
+        listReviews = new ArrayList<>();
+        listReviews.add(new writeReviewsModel("Staff 1", "Customer 1", "Title 1", "Description1", 5, "Date Daw"));
+        adapterReviews = new reviews_adapter(listReviews, this);
+        rvReviews.setAdapter(adapterReviews);
+
+
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
